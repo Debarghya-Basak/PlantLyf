023f748b38f7cdef1b3a33c7291c0effbdf33b70
Debarghya Basak <debarghyabasakofficial.2001@gmail.com>
2024-04-13 18:05:07 +0530

    Added real plant image prediction

--- app/src/main/java/com/db/plantlyf/AiModelHandler/PlantDiseaseClassifier.java
+++ app/src/main/java/com/db/plantlyf/AiModelHandler/PlantDiseaseClassifier.java
@@ -1,4 +1,114 @@
 package com.db.plantlyf.AiModelHandler;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
 public class PlantDiseaseClassifier {
+
+    private Interpreter tfliteInterpreter;
+    private int inputImageWidth;
+    private int inputImageHeight;
+    private ByteBuffer inputBuffer;
+    private final String[] labels;
+    private final int numLabels;
+
+    public PlantDiseaseClassifier(Context context, int inputImageWidth, int inputImageHeight, String[] labels) throws IOException {
+        this.inputImageWidth = inputImageWidth;
+        this.inputImageHeight = inputImageHeight;
+        this.labels = labels;
+        this.numLabels = labels.length;
+
+        // Load the TFLite model from the assets folder
+        tfliteInterpreter = new Interpreter(loadModelFile(context));
+
+        // Initialize the input ByteBuffer
+        int bufferSize = inputImageWidth * inputImageHeight * 3 * Float.SIZE / Byte.SIZE; // 3 for RGB channels
+        inputBuffer = ByteBuffer.allocateDirect(bufferSize);
+        inputBuffer.order(ByteOrder.nativeOrder());
+
+        // Print the input tensor shape
+        int[] inputShape = tfliteInterpreter.getInputTensor(0).shape();
+        Log.d("Input shape", "Input Tensor Shape: " + Arrays.toString(inputShape));
+    }
+
+    private ByteBuffer loadModelFile(Context context) throws IOException {
+
+        // Load the TFLite model from the assets folder
+        InputStream inputStream = context.getAssets().open("plant_disease_classifier.tflite");
+        int modelFileSize = inputStream.available();
+        ByteBuffer buffer = ByteBuffer.allocateDirect(modelFileSize);
+        byte[] modelData = new byte[modelFileSize];
+        inputStream.read(modelData);
+        buffer.put(modelData);
+        buffer.rewind();
+        inputStream.close();
+        return buffer;
+
+    }
+
+    public String classifyImage(Bitmap bitmap) {
+        // Resize the input image to the model input shape
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, inputImageWidth, inputImageHeight, true);
+
+        //imageView.setImageBitmap(resizedBitmap);
+        // Normalize the pixel values to [0, 1] and load them into the input ByteBuffer
+        inputBuffer.rewind();
+
+        int c = 0;
+        for (int y = 0; y < inputImageHeight; y++) {
+            for (int x = 0; x < inputImageWidth; x++) {
+                int pixelValue = resizedBitmap.getPixel(x, y);
+
+                // Extract RGB channels and normalize to [0, 1]
+                float red = ((pixelValue >> 16) & 0xFF);
+                float green = ((pixelValue >> 8) & 0xFF);
+                float blue = (pixelValue & 0xFF);
+
+                inputBuffer.putFloat(red);
+                inputBuffer.putFloat(green);
+                inputBuffer.putFloat(blue);
+                if(c < 20)
+                    Log.d("Image float values", red + "," + green + "," + blue);
+
+                c++;
+            }
+        }
+
+        // Run inference
+        float[][] output = new float[1][numLabels];
+        tfliteInterpreter.run(inputBuffer, output);
+
+        // Process the output and get the predicted label
+        return processOutput(output);
+    }
+
+    private String processOutput(float[][] output) {
+        int maxIndex = 0;
+        float maxProbability = output[0][0];
+        for (int i = 0; i < output[0].length; i++) {
+            Log.d("Predicted output : ", output[0][i]+"");
+            if (output[0][i] > maxProbability) {
+                maxProbability = output[0][i];
+                maxIndex = i;
+            }
+        }
+
+        // Return the predicted label based on maxIndex
+        return labels[maxIndex];
+    }
+
+    // Close the interpreter to release resources
+    public void close() {
+        tfliteInterpreter.close();
+    }
+
 }
---
+++ app/src/main/java/com/db/plantlyf/ScanPlantDisease.java
@@ -0,0 +1,181 @@
+package com.db.plantlyf;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.db.plantlyf.AiModelHandler.SoilTypeClassifier;
+import com.db.plantlyf.Utilities.DarkModeStatus;
+import com.db.plantlyf.Utilities.DialogBox;
+import com.db.plantlyf.databinding.ActivityScanPlantDiseaseBinding;
+
+import java.io.IOException;
+
+public class ScanPlantDisease extends AppCompatActivity {
+
+    private ActivityScanPlantDiseaseBinding binding;
+    private final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityScanPlantDiseaseBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+
+        initializePage();
+
+    }
+
+    private void initializePage() {
+        initializeButton();
+        initializeContainerBg();
+        initializeRecommendedPlantListLL();
+        startBgVideo();
+    }
+
+    private void initializeButton(){
+        binding.captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                binding.predictedPlantDiseaseTV.setAlpha(0);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+
+            }
+        });
+    }
+
+    private void predictPlantDisease(Intent data) throws IOException {
+
+        Bundle extras = data.getExtras();
+        Bitmap soilImage = (Bitmap) extras.get("data");
+        Bitmap resizedSoilImage = resizeBitmap(soilImage, 200, 200);
+
+        binding.capturePlantDiseasePreviewIV.setImageBitmap(resizedSoilImage);
+
+        SoilTypeClassifier soilTypeClassifier;
+
+        int inputImageWidth = 256;
+        int inputImageHeight = 256;
+        String[] labels = {"Alluvial", "Black", "Clay", "Red"};
+
+        soilTypeClassifier = new SoilTypeClassifier(this, inputImageWidth, inputImageHeight, labels);
+
+        String predictedLabel = soilTypeClassifier.classifyImage(resizedSoilImage);
+
+        DialogBox predictionDialogBox = new DialogBox(this, R.layout.global_prediction_dialog_box);
+        predictionDialogBox.showDialog();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                predictionDialogBox.dismissDialog();
+                binding.predictedPlantDiseaseTV.setText("It is " + predictedLabel + " soil");
+                binding.predictedPlantDiseaseTV.animate().alpha(1).start();
+            }
+        }, 2500);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+
+            if(data != null) {
+                try {
+                    predictPlantDisease(data);
+                } catch (IOException e) {}
+            }
+            else
+                Toast.makeText(this, "No Image Captured", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private Bitmap resizeBitmap(Bitmap soilImage, int width, int height) {
+        return Bitmap.createScaledBitmap(soilImage, convertDpToPixels(width), convertDpToPixels(height), false);
+    }
+
+    private int convertDpToPixels(int dp) {
+        return (int)(dp * Resources.getSystem().getDisplayMetrics().density);
+
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private void initializeRecommendedPlantListLL() {
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float height = displayMetrics.heightPixels;
+        float width = displayMetrics.widthPixels;
+
+        ViewGroup.LayoutParams layoutParams = binding.recommendedSolutionContainerLL.getLayoutParams();
+        layoutParams.height = (int)((height * 35f)/100f);
+
+        if(DarkModeStatus.isDarkModeEnabled(this))
+            binding.captureImageIV.setBackground(getDrawable(R.drawable.global_capture_icon_dark));
+        else
+            binding.captureImageIV.setBackground(getDrawable(R.drawable.global_capture_icon_light));
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private void initializeContainerBg() {
+
+        if(DarkModeStatus.isDarkModeEnabled(this))
+            binding.containerBgV.setBackground(getDrawable(R.drawable.global_container_bg_dark));
+        else
+            binding.containerBgV.setBackground(getDrawable(R.drawable.global_container_bg_light));
+
+    }
+
+    private void startBgVideo() {
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float height = displayMetrics.heightPixels;
+        float width = displayMetrics.widthPixels;
+        //Toast.makeText(this, dpHeight + "," + dpWidth, Toast.LENGTH_SHORT).show();
+
+        VideoView videoView = binding.dashboardBgVV;
+        ViewGroup.LayoutParams layoutParams = videoView.getLayoutParams();
+        layoutParams.width = (int)width + ((int)((width * 37.0) / 100.0));
+        videoView.setLayoutParams(layoutParams);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.plantlyfbganim);
+        videoView.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                videoView.setVisibility(View.VISIBLE);
+            }
+        },500);
+
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                videoView.start();
+            }
+        });
+    }
+
+}
